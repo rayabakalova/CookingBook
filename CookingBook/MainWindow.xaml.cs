7e35b8029633d88db62bf727666232f305d5b1f2
Rosen Karadinev <karadinev@gmail.com>
2016-05-15 19:32:43 +0300

    Database Connected to the project

--- CookingBook/MainWindow.xaml.cs
+++ CookingBook/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CookingBook.Objects;
+
 
 namespace CookingBook
 {
@@ -20,9 +22,36 @@ namespace CookingBook
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private MainWindowVeiwModel db = new MainWindowVeiwModel();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //db.FillRecipe();
+
+            LoadMenu();
+
+
+            //Test.Content = db.Recipe[0].recipe_name;
+
+
+        }
+
+        private void LoadMenu()
+        {
+            db.FillCategories();
+            Menu.ItemsSource = db.Category;
+        }
+
+        private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var item = sender as ListViewItem;
+            if (item != null)
+            {
+                Test.Content = "Raboti";
+            }
         }
     }
 }
---
+++ CookingBook/Objects/MainWindowViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CookingBook.Objects
+{
+    class MainWindowVeiwModel : INotifyPropertyChanged
+    {
+
+        CookingDataEntities cbdb = new CookingDataEntities();
+
+        public void FillRecipe()
+        {
+            var q = (from recipe in cbdb.Recipes
+                     orderby recipe.recipe_name
+                     select recipe).ToList();
+
+            this.Recipe = q;
+        }
+
+        public void FillCategories()
+        {
+            var q = (from category in cbdb.Categories
+                     orderby category.category_id
+                     select category).ToList();
+
+            this.Category = q;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        private List<Recipe> _recipe;
+        public List<Recipe> Recipe
+        {
+            get
+            {
+                return _recipe;
+            }
+            set
+            {
+                _recipe = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private List<Category> _category;
+        public List<Category> Category
+        {
+            get
+            {
+                return _category;
+            }
+            set
+            {
+                _category = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private List<Product> _products;
+        public List<Product> Products
+        {
+            get
+            {
+                return _products;
+            }
+            set
+            {
+                _products = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
