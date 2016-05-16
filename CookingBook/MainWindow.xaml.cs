fb9c8ebf4682dc235e787225c4d8e625b4e449d2
Rosen Karadinev <karadinev@gmail.com>
2016-05-16 21:45:58 +0300

    add recommended recipes

--- CookingBook/MainWindow.xaml.cs
+++ CookingBook/MainWindow.xaml.cs
@@ -25,13 +25,17 @@ namespace CookingBook
 
         private MainWindowVeiwModel db = new MainWindowVeiwModel();
 
+        private static string postfix = ".jpg";
+        private static string PathBase = "/CookingBook;component/Images/Dishes/";
+
         public MainWindow()
         {
             InitializeComponent();
 
-            //db.FillRecipe();
+            db.FillRecipe();
 
             LoadMenu();
+            LoadRecommendedImages();
 
 
             //Test.Content = db.Recipe[0].recipe_name;
@@ -43,6 +47,63 @@ namespace CookingBook
         {
             db.FillCategories();
             Menu.ItemsSource = db.Category;
+        }
+
+        private void LoadRecommendedImages()
+        {
+            Random rnd = new Random();
+            string path = "";
+            string name = "";
+
+            for (int i = 0; i < 4; i++)
+            {
+                int index = rnd.Next(0, 2);
+
+                if (db.Recipe[index] != null)
+                {
+                    path = PathBase + db.Recipe[index].recipe_id + postfix;
+                    name = db.Recipe[index].recipe_name;
+                }
+
+                switch (i)
+                {
+                    case 1:
+                        {
+                            SetImage(RecommendedImg1, path);
+                            RecommendedTxt1.Content = name;
+                        } 
+                        break;
+                    case 2:
+                        {
+                            SetImage(RecommendedImg2, path);
+                            RecommendedTxt2.Content = name;
+                        }
+                        break;
+                    case 3:
+                        {
+                            SetImage(RecommendedImg3, path);
+                            RecommendedTxt3.Content = name;
+                        }
+                        break;
+                    default:
+                        break;
+                }       
+            }
+        }
+
+        private void SetImage(Image img ,string path)
+        {
+            Uri uri = null;
+
+            try
+            {
+                uri = new Uri("pack://application:,,," + path);
+            }
+            finally
+            {
+                img.Source = new BitmapImage(uri);
+            }
+           
         }
 
         private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
